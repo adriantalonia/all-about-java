8b4056933dbf11305caedcb767d68e8ff7abe6c4
Adrian <adrian.talonia11@gmail.com>
2023-05-15 23:16:35 -0600

    add reverse array hackerrank

---
+++ src/_004_interview_preparation/_05_hackerrank/_01_arrays/ReverseArray.java
@@ -0,0 +1,16 @@
+package _004_interview_preparation._05_hackerrank._01_arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReverseArray {
+
+    public static List<Integer> reverseArray(List<Integer> a) {
+        // Write your code here
+        List<Integer> res = new ArrayList<>();
+        for(int i = a.size()-1; i >= 0; i--) {
+            res.add(a.get(i));
+        }
+        return res;
+    }
+}
